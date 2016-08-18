dd0a15170730d857cf8fa6e6aab8873e11a28131
dong.zhang <dong.zhang@EP-107-16D1>
2016-08-18 08:36:03 -0600

    Add project files.

---
+++ Ch04Ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+using static System.Convert;
+
+namespace Ch04Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "benjamin";
+            const string niceName = "andrea";
+            const string sillyName = "ploppy";
+            string name;
+            WriteLine("What is your name?");
+        }
+    }
+}
